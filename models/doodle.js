40e679812d0e50be728fa4fc8ab7bffb230e047f
Josh Lapham <josh@joshlapham.com>
2016-07-31 08:00:51 +1000

    Remove sqlString function from models.

--- models/doodle.js
+++ models/doodle.js
@@ -1,13 +1,8 @@
 var db = require('../db.js')
 
-function safeSQL() {
-  var sqlString = 'SELECT image_url, instagram_id, date, image_id FROM doodles WHERE is_active=1 ORDER BY image_id'
-  return sqlString
-}
-
 exports.getAll = function(done) {
   // Finds all doodles and calls `done(null, doodles)`
-  var sqlString = safeSQL()
+  var sqlString = 'SELECT image_url, instagram_id, date, image_id FROM doodles WHERE is_active=1 ORDER BY image_id'
 
   db.get().query(sqlString, function(err, doodles) {
     if (err) {
--- models/video.js
+++ models/video.js
@@ -1,13 +1,8 @@
 var db = require('../db.js')
 
-function safeSQL() {
-  var sqlString = 'SELECT name, description, duration, date, youtube_id FROM videos WHERE is_active=1 ORDER BY date'
-  return sqlString
-}
-
 exports.getAll = function(done) {
   // Finds all videos and calls `done(null, videos)`
-  var sqlString = safeSQL()
+  var sqlString = 'SELECT name, description, duration, date, youtube_id FROM videos WHERE is_active=1 ORDER BY date'
 
   db.get().query(sqlString, function(err, videos) {
     if (err) {
